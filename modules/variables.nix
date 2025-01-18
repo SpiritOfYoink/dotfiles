4bffbd33c6db4be45d0b31fa2057a25adefb97fa
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-18 01:02:54 -0800

    Update variables.nix

--- modules/variables.nix
+++ modules/variables.nix
@@ -1,7 +1,7 @@
     { lib, ... }:
     
     #   ..... VARIABLES .....    
-    
+
       options.user = "yoink";     # What's your login?
       fullname = "The Spirit of Yoink!";      # What's the user called?
       hostname = "Ncase M2";      # What's the computer called?
@@ -10,7 +10,7 @@
       #rootpw = mkOption {};      # What is the root user's secret file?
 
       server = "//192.168.1.70/NAS_Storage";      # Where's your network storage attached? (SMB share.)
-      github = "github:SpiritOfYoink/dotfiles";       # Change this to the github link for your repository.
+      github = "https://github.com/SpiritOfYoink/dotfiles";       # Change this to the github link for your repository.
 
       system = "x86_64-linux";        # This doesn't need to change unless you're using ARM or Apple silicon.
       pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";    # If 'system' changes, change this!
