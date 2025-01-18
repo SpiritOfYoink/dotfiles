e13378850311eb5f7645e42700cc83e01e442626
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-18 05:22:10 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,7 @@
 
 #   ..... INPUTS .....
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";       # Nixpkgs.
-    # nix.package = pkgs.nixVersions.latest;   # Prevents NixOS from throwing an error about nixVersions.unstable
-    
-    home-manager.url = "github:nix-community/home-manager";    # Home manager.
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
- 
-    nix-colors.url = "github:misterio77/nix-colors";    # Nix colors.
 
-    sops-nix.url = "github:mic92/sops-nix";   # Secret provisioning for password security.
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    
-    nix-gl.url = "github:nix-community/nixgl";    # Wrapper to fix launching openGL games.
-    nix-gl.inputs.nixpkgs.follows = "nixpkgs";
-  
-    niri-stable.url = "github:YaLTeR/niri/v25.01";    # Niri window manager.
-    niri-stable.inputs.nixpkgs.follows = "nixpkgs";
-
-    xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.5";   # Allows Niri to hook into wayland.
-    xwayland-satellite-stable.inputs.nixpkgs.follows = "nixpkgs";
-    };
 
 
 #   ..... OUTPUTS .....
@@ -65,4 +45,26 @@
           };
         };
       };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";       # Nixpkgs.
+    # nix.package = pkgs.nixVersions.latest;   # Prevents NixOS from throwing an error about nixVersions.unstable
+    
+    home-manager.url = "github:nix-community/home-manager";    # Home manager.
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+ 
+    nix-colors.url = "github:misterio77/nix-colors";    # Nix colors.
+
+    sops-nix.url = "github:mic92/sops-nix";   # Secret provisioning for password security.
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    
+    nix-gl.url = "github:nix-community/nixgl";    # Wrapper to fix launching openGL games.
+    nix-gl.inputs.nixpkgs.follows = "nixpkgs";
+  
+    niri-stable.url = "github:YaLTeR/niri/v25.01";    # Niri window manager.
+    niri-stable.inputs.nixpkgs.follows = "nixpkgs";
+
+    xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.5";   # Allows Niri to hook into wayland.
+    xwayland-satellite-stable.inputs.nixpkgs.follows = "nixpkgs";
+    };
 }
