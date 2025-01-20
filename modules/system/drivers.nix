a5aad1dda1fb0f877f4edc16c0c1e02eb269a8a4
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-20 00:53:28 -0800

    Update drivers.nix

--- modules/system/drivers.nix
+++ modules/system/drivers.nix
@@ -14,9 +14,7 @@ in {
   config = mkIf cfg.drivers.enable (mkMerge [
 
     hardware = {
-
-      enableRedistributableFirmware = true;   # Enables firmware with a licence allowing redistribution.
-
+      enableAllFirmware = true;   # Enables firmware with a licence allowing redistribution.
       graphics = {
         enable = true;    # Enables OpenGL.
         enable32Bit = true;   # Enables 32-bit drivers for 32-bit applications (such as Wine).
