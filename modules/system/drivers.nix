f222260aea2b32d3d5dc719c40c9aebe221c578e
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-20 01:05:05 -0800

    Update drivers.nix

--- modules/system/drivers.nix
+++ modules/system/drivers.nix
@@ -11,9 +11,8 @@ in {
     };
 
 #   ..... CONFIG .....
-  config = {
-
-  mkIf (cfg.drivers.enable = true) = {
+  config = mkMerge [
+    (mkIf cfg.drivers.enable = {
     hardware = {
       enableAllFirmware = true;   # Enables firmware with a licence allowing redistribution.
       graphics = {
@@ -21,28 +20,28 @@ in {
         enable32Bit = true;   # Enables 32-bit drivers for 32-bit applications (such as Wine).
         driSupport = true;    # Enables Vulkan .
         driSupport32Bit = true; 
-        }; }; };
+        }; }; }; );
 
-    mkIf (cfg.nvidia-drivers.enable = true && cfg.drivers.enable = true) = {
+    ( mkIf cfg.nvidia-drivers.enable = {
 
-    nixpkgs.config.allowUnfree = true;
-    services.xserver.videoDrivers = ["nvidia"];   # Loads Nvidia driver for Xorg and Wayland.
+      nixpkgs.config.allowUnfree = true;
+      services.xserver.videoDrivers = ["nvidia"];   # Loads Nvidia driver for Xorg and Wayland.
 
-    hardware.nvidia = {
-      open = true;    # Uses the open-source modules (not drivers).
-      modesetting.enable = true;    # Modesetting is required to run wayland.
+      hardware.nvidia = {
+        open = true;    # Uses the open-source modules (not drivers).
+        modesetting.enable = true;    # Modesetting is required to run wayland.
 
-#      nvidia-persistenced = true;    # Allows the toggling of 'persistence mode' in nvidia management software. May only be for laptops?
-#      dynamicBoost.enable = true;    # Allows the GPU clock speed to boost as normal. May only be for laptops?
+  #      nvidia-persistenced = true;    # Allows the toggling of 'persistence mode' in nvidia management software. May only be for laptops?
+  #      dynamicBoost.enable = true;    # Allows the GPU clock speed to boost as normal. May only be for laptops?
 
-      powerManagement = {
-        enable = false;   # Experimental and can cause sleep/suspend to fail. Enable if having crashes after wakie from sleep.
-        finegrained = false;    # Turns off GPU when not in use. Do not enable.
-        };
+        powerManagement = {
+          enable = false;   # Experimental and can cause sleep/suspend to fail. Enable if having crashes after wakie from sleep.
+          finegrained = false;    # Turns off GPU when not in use. Do not enable.
+          };
 
-      nvidiaSettings = true;    # Enables the nvidia settings menu.
-      package = config.boot.kernelPackages.nvidiaPackages.latest;   # Use most recent Nvidia drivers.
-      }; }; };
-      
+        nvidiaSettings = true;    # Enables the nvidia settings menu.
+        package = config.boot.kernelPackages.nvidiaPackages.latest;   # Use most recent Nvidia drivers.
+        }; }; ); ];
+
 
 }   # End of file.
