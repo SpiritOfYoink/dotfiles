34a604155316db41b4846428a2658d8644351fbc
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-20 20:40:01 -0800

    Update boot.nix

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -6,12 +6,11 @@
 #   ..... CONFIG .....
 
   config = mkIf boot.enable {
-    boot = { 
-      loader = {
+    boot.loader = {
         systemd-boot.enable = true;   # Enables bootloader. DO NOT TOUCH.
         efi.canTouchEfiVariables = true;    # Allows bootloader to manage boot entries. DO NOT TOUCH.
         systemd-boot.configurationLimit = 10;   # Limits the number of previous configurations stored in the bootloader. Increase if you may need to go further back in time.
-        }; };
+        };
 
   config = mkIF boot.enable && nvidia-drivers.enable {
     boot.blacklistedKernelModules = [ "nouveau"];    # Prevents the open-source drivers from loading.
