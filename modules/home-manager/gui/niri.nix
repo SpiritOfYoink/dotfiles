dc51b948a70dde79b284b973093a30c9cfccfe00
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-20 20:03:57 -0800

    moving gui to home-manager

--- modules/home-manager/gui/niri.nix
+++ modules/home-manager/gui/niri.nix
@@ -72,7 +72,12 @@ in {
             };
 
           default-column-width = { proportion = 0.33333; };
-          };
+
+          preset-column-widths = {
+            proportion = 0.33333;
+            proportion = 0.5;
+            proportion = 0.66667;
+            }; };
 
 
         #   ..... RULES .....
