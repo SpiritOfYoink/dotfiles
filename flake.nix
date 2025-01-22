ada694746a6f2faf36bcc9a8d843b6fc8184534e
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-22 12:32:07 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,72 +1,75 @@
 {  description = "Home Manager Configuration";
 
 #   ..... INPUTS .....
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";       # Nixpkgs.
-    home-manager.url = "github:nix-community/home-manager";    # Home manager.
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-   # wsl.url = "github:nix-community/NixOS-WSL";    # Used for Windows Subsystem for Linux compatibility
-   # nix-colors.url = "github:misterio77/nix-colors";    # Nix colors.
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-      };
-   # sops-nix.url = "github:mic92/sops-nix";   # Secret provisioning for password security.
-   # nix-gl.url = "github:nix-community/nixgl";    # Wrapper to fix launching openGL games.
-    niri-stable.url = "github:YaLTeR/niri/v25.01";    # Niri window manager.
-    niri-stable.inputs.nixpkgs.follows = "nixpkgs";
-   # xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.5";   # Allows Niri to hook into wayland.
+    inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";       # Nixpkgs.
+      home-manager.url = "github:nix-community/home-manager";    # Home manager.
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # wsl.url = "github:nix-community/NixOS-WSL";    # Used for Windows Subsystem for Linux compatibility
+    # nix-colors.url = "github:misterio77/nix-colors";    # Nix colors.
+      stylix = {
+        url = "github:danth/stylix";
+        inputs.nixpkgs.follows = "nixpkgs";
+        inputs.home-manager.follows = "home-manager";
+        };
+    # sops-nix.url = "github:mic92/sops-nix";   # Secret provisioning for password security.
+    # nix-gl.url = "github:nix-community/nixgl";    # Wrapper to fix launching openGL games.
+      niri-stable.url = "github:YaLTeR/niri/v25.01";    # Niri window manager.
+      niri-stable.inputs.nixpkgs.follows = "nixpkgs";
+    # xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.5";   # Allows Niri to hook into wayland.
     };
 
 
 #   ..... OUTPUTS .....
-  outputs = {self, nixpkgs, home-manager, stylix, niri-stable, ... }@inputs:
+    outputs = {self, nixpkgs, home-manager, stylix, niri-stable, ... }@inputs:
+      let
+        inherit (home-manager.lib) homeManagerConfiguration;
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true; };
+          };
 
-    let
-      inherit (home-manager.lib) homeManagerConfiguration;
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {allowUnfree = true; };
-        };
+      in rec {
+        packages = [
+          import ./pkgs { inherit pkgs; }
+          pkgs.home-manager
+          ];
+      
+      
 
-    in  {
+  #   ..... HOST SETUPS .....
 
-    environment.systemPackages = [    # These programs will be installed system-wide.
-      pkgs.home-manager
-      ];
+      homeConfigurations = {       
 
-#   ..... HOST SETUPS .....
+        yoink = home-manager.lib.homeManagerConfiguration {   # THE SPIRIT OF YOINK
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          specialArgs = { inherit inputs outputs; };   # Allows modules access to flake inputs.
+          extraSpecialArgs = { inherit inputs outputs; };   # Allows home-manager modules access to flake inputs.
+          config = { allowUnfree = true; };
+          modules = [ ./users/yoink ];
+          };
 
-    homeConfigurations = {       
+          };
 
-      yoink = home-manager.lib.homeManagerConfiguration {   # THE SPIRIT OF YOINK
-        specialArgs = { inherit inputs; inherit pkgs; };   # The `specialArgs` parameter passes the non-default nixpkgs instances to other nix modules
-        config = { allowUnfree = true; };
-        modules = [ ./users/yoink ];
-        };
+  #     dame = nixpkgs.lib.nixosSystem rec {    # NO AIM DAME
+  #       specialArgs = { inherit inputs; };
+  #         modules = [ ./hosts/dame/configuration.nix ./home-manager ];
+  #         };
 
-        };
+  #    mac = nixpkgs.lib.nixosSystem rec {   # MAC'N'CHEESE
+  #       specialArgs = { inherit inputs; };
+  #         modules = [ ./hosts/mac/configuration.nix ./home-manager ];
+  #         };
 
- #     dame = nixpkgs.lib.nixosSystem rec {    # NO AIM DAME
- #       specialArgs = { inherit inputs; };
- #         modules = [ ./hosts/dame/configuration.nix ./home-manager ];
- #         };
+  #     hamster = nixpkgs.lib.nixosSystem rec {    # HAMSTER
+  #       specialArgs = { inherit inputs; };
+  #         modules = [ ./hosts/hamster/configuration.nix ./home-manager ];
+  #         };
 
- #    mac = nixpkgs.lib.nixosSystem rec {   # MAC'N'CHEESE
- #       specialArgs = { inherit inputs; };
- #         modules = [ ./hosts/mac/configuration.nix ./home-manager ];
- #         };
+  #     server = nixpkgs.lib.nixosSystem rec {    # SERVER
+  #       specialArgs = { inherit inputs; };
+  #         modules = [ ./hosts/server/configuration.nix ./home-manager ];
+  #         };
 
- #     hamster = nixpkgs.lib.nixosSystem rec {    # HAMSTER
- #       specialArgs = { inherit inputs; };
- #         modules = [ ./hosts/hamster/configuration.nix ./home-manager ];
- #         };
-
- #     server = nixpkgs.lib.nixosSystem rec {    # SERVER
- #       specialArgs = { inherit inputs; };
- #         modules = [ ./hosts/server/configuration.nix ./home-manager ];
- #         };
-
-    }; }   # End of file.
+        }; }   # End of file.
