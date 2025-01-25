0b32da8e4d5261e728a526459181cb3bc2ac39f5
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-24 23:46:42 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@ description = "Home Manager Configuration";
         pkgs = nixpkgs.legacyPackages.${system};
       in {
 
-      environment.systemPackages = with pkgs: [
+      environment.systemPackages = with pkgs; [
         git   # Flakes clones its dependencies through the git command, so it must be at the top of the list.
         home-manager
         ];
