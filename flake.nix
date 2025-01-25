1da8ae92a4451e8290ea3ef867f8e9b74aba2f2d
SpiritOfYoink <thespiritofyoink@gmail.com>
2025-01-24 16:15:57 -0800

    no outputs

--- flake.nix
+++ flake.nix
@@ -88,8 +88,8 @@
 
         yoink = home-manager.lib.homeManagerConfiguration {   # THE SPIRIT OF YOINK
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          specialArgs = { inherit inputs outputs; };   # Allows modules access to flake inputs.
-          extraSpecialArgs = { inherit inputs outputs; };   # Allows home-manager modules access to flake inputs.
+          specialArgs = { inherit inputs; };   # Allows modules access to flake inputs.
+          extraSpecialArgs = { inherit inputs; };   # Allows home-manager modules access to flake inputs.
           config = { allowUnfree = true; };
           modules = [ ./users/yoink ];
           };
